764c498d539d0b4ff597d9742da4a0c69df6622e
salmansaputra17 <salmansaputra76@gmail.com>
2020-07-07 21:43:58 +0700

    add some controllers

---
+++ api/controllers/login_controller.go
@@ -0,0 +1,11 @@
+package controllers
+
+import "net/http"
+
+func (server *Server) Login(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (server *Server) SignIn(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ api/controllers/posts_controller.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "net/http"
+
+func (server *Server) CreatePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (server *Server) GetPosts(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (server *Server) GetPost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (server *Server) UpdatePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (server *Server) DeletePost(w http.ResponseWriter, r *http.Request) {
+
+}
